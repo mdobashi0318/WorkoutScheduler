2d2e9ce82647f9c937a06f03ef4b0e9c5ba9842d
MasaharuDobashi <dobashi.masaharu@gmail.com>
2025-09-22 12:50:39 +0900

    HistoryListScreenの遷移追加

--- WorkoutScheduler/Screens/Workout/HistoryListScreen.swift
+++ WorkoutScheduler/Screens/Workout/HistoryListScreen.swift
@@ -12,6 +12,8 @@ struct HistoryListScreen: View {
     
     @Query private var historys: [History] = []
     
+    @Environment(\.modelContext) private var modelContext
+    
     private let dispSec: Int = 30
     
     init() {
@@ -28,12 +30,15 @@ struct HistoryListScreen: View {
             ZStack {
                 List {
                     ForEach(historys) { history in
-                        VStack(alignment: .leading) {
-                            Text(history.workout?.name ?? "")
-                            Text(history.timeElapsed())
-                            Text(history.workoutTime())
-                        }
+                        NavigationLink(value: history.workout, label: {
+                            VStack(alignment: .leading) {
+                                Text(history.workout?.name ?? "")
+                                Text(history.timeElapsed())
+                                Text(history.workoutTime())
+                            }
+                        })
                     }
+                    .onDelete(perform: delete)
                 }
                 .navigationTitle("HistoryListScreen")
                 
@@ -42,8 +47,20 @@ struct HistoryListScreen: View {
                         .font(.subheadline)
                 }
             }
+            .navigationDestination(for: Workout.self, destination: {
+                WorkoutDetailScreen(workout: $0)
+            })
         }
     }
+    
+    private func delete(at offsets: IndexSet) {
+        for offset in offsets {
+            let history = historys[offset]
+            modelContext.delete(history)
+            try? modelContext.save()
+        }
+    }
+    
 }
 
 #Preview {
